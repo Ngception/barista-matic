79fda962625544bb98b06433cf68ac7852b61d04
Patrick <113420241+Ngception@users.noreply.github.com>
2022-09-14 21:07:43 -0400

    refactor: remove imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../app/store';
-import App from '../App';
+import App from './App';
 
 test('App successfully loads', () => {
   render(
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
   );
 
